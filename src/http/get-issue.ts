3fe1c8da44e198d09dbb5bfc4cf0d2bb7f59ae2f
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-06-15 11:07:11 -0300

    feat(issues): create main skeleton   Added the main structure for the page issues to use a skeleton loader.

--- src/http/get-issue.ts
+++ src/http/get-issue.ts
@@ -1,3 +1,5 @@
+import { setTimeout } from 'node:timers/promises'
+
 import { IssueSchema } from '@/api/routes/get-issue'
 import { clientEnv } from '@/env'
 
@@ -6,6 +8,8 @@ interface GetIssueParams {
 }
 
 export async function getIssue({ id }: GetIssueParams) {
+  await setTimeout(2000)
+
   const url = new URL(`/api/issues/${id}`, clientEnv.NEXT_PUBLIC_API_URL)
 
   const response = await fetch(url)
